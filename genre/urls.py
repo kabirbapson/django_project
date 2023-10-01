1b689a5f272601fdf6c7be35137587109b97b5d1
Bappi Kabir <kabirbapson@gmail.com>
2023-10-01 23:35:20 +0100

    first init

--- genre/urls.py
+++ genre/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # genre/
+    path('', views.index, name='index'),
+
+    #genre/1
+    path('<int:genre_id>', views.details, name='details')
 ]
--- genre/views.py
+++ genre/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
+
 def index(response):
     return HttpResponse('<h1>Hello baps hhere</h1>')
+
+def details(response, genre_id):
+    return HttpResponse('<h2>Genre ID is: ' + str(genre_id) + '</h2>')
