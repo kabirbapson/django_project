705b4893560edb2b00a52e6076722ce3ae098aa1
Bappi Kabir <kabirbapson@gmail.com>
2023-10-02 08:09:48 +0100

    first init

--- genre/views.py
+++ genre/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Collections, Piece
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse('<h1>Hello baps hhere</h1>')
+def index(request):
+    all_collection = Collections.objects.all()
+    bello = 'Halima Bello'
+    context = {
+        'all_collection': all_collection,
+        'bello': bello,
+    }
+    # print(all_collection)  # Debugging: Print the contents of all_collection
+    return render(request, "genre/index.html", context)
 
-def details(response, genre_id):
-    return HttpResponse('<h2>Genre ID is: ' + str(genre_id) + '</h2>')
+def details(request, genre_id):
+    cItem = Collections.objects.get(pk=genre_id)
+    pItem = Piece.objects.filter(collection = cItem)
+    context = {
+        'pItem': pItem
+    }
+    print('my doe ',pItem)
+    return render(request, 'genre/details.html', context)
