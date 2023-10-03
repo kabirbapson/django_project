48a62c50c6d2a62d03e88c823c8dde45ee17414d
Bappi Kabir <kabirbapson@gmail.com>
2023-10-03 03:00:35 +0100

    first init

--- genre/urls.py
+++ genre/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     # genre/
-    path('', views.index, name='index'),
+    path('', views.index.as_view(), name='index'),
 
     #genre/1
-    path('<int:genre_id>', views.details, name='details')
+    # path('<int:genre_id>', views.details, name='details')
+    path('<pk>/', views.details.as_view(), name='details')
 ]
--- genre/views.py
+++ genre/views.py
@@ -1,24 +1,36 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Collections, Piece
-
+from django.views import generic
 # Create your views here.
 
-def index(request):
-    all_collection = Collections.objects.all()
-    bello = 'Halima Bello'
-    context = {
-        'all_collection': all_collection,
-        'bello': bello,
-    }
-    # print(all_collection)  # Debugging: Print the contents of all_collection
-    return render(request, "genre/index.html", context)
+class index(generic.ListView):
+    template_name = 'genre/index.html'
 
-def details(request, genre_id):
-    cItem = Collections.objects.get(pk=genre_id)
-    pItem = Piece.objects.filter(collection = cItem)
-    context = {
-        'pItem': pItem
-    }
-    print('my doe ',pItem)
-    return render(request, 'genre/details.html', context)
+    def get_queryset(self):
+        return Collections.objects.all()
+    
+class details(generic.DetailView):
+    model = Collections
+    template_name = 'genre/details.html'
+
+# def index(request):
+#     all_collection = Collections.objects.all()
+#     bello = 'Halima Bello'
+#     context = {
+#         'all_collection': all_collection,
+#         'bello': bello,
+#     }
+#     # print(all_collection)  # Debugging: Print the contents of all_collection
+#     return render(request, "genre/index.html", context)
+
+# def details(request, genre_id):
+#     cItem = Collections.objects.get(pk=genre_id)
+#     pItem = Piece.objects.filter(collection = cItem)
+#     context = {
+#         'pItem': pItem
+#     }
+#     print('my doe ',pItem)
+#     return render(request, 'genre/details.html', context)
